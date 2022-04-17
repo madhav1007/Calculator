96f583e5b01d914c2279f60708ea1caeac183875
madhav1007 <satyamadhavig1007@gmail.com>
2022-04-17 08:05:37 +0530

    added 1st commit

---
+++ src/calc/CalculatorClass.java
@@ -0,0 +1,12 @@
+package calc;
+
+public class CalculatorClass {
+
+	public static void main(String[] args) {
+		System.out.println("Sum of two numbers is " + sum(10, 20));
+	}
+
+	public static int sum(int a, int b) {
+		return a + b;
+	}
+}
