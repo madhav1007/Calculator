2bbadde8153696a11ec497a3dc68b6ca040f3236
madhav1007 <satyamadhavig1007@gmail.com>
2022-04-17 08:29:48 +0530

    Added difference method in Calculator class

--- src/calc/CalculatorClass.java
+++ src/calc/CalculatorClass.java
@@ -4,9 +4,14 @@ public class CalculatorClass {
 
 	public static void main(String[] args) {
 		System.out.println("Sum of two numbers is " + sum(10, 20));
+		System.out.println("Sum of two numbers is " + difference(100, 20));
 	}
 
 	public static int sum(int a, int b) {
 		return a + b;
+	}
+	
+	public static int difference(int a, int b) {
+		return a - b;
 	}
 }
